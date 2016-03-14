0fd0a76d0a75fa3259362edc390b2467e57c4005
Rylan Hawkins <RylanH@gmail.com>
2016-03-14 07:58:37 -0700

    Initial checkin

---
+++ webpack.config.dev.js
@@ -0,0 +1,40 @@
+var path          = require('path');
+var webpack       = require('webpack');
+var autoprefixer  = require('autoprefixer');
+var precss        = require('precss');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [  
+      {
+        test: /\.jsx?/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      { 
+        test: /\.css/, 
+        loader: "style-loader!css-loader!postcss-loader" 
+      }
+    ],
+  },
+
+  postcss: function () {
+    return [autoprefixer, precss];
+  }
+};
